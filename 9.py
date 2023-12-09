68c9f0b072d98159d55047f0f12cbf33faf1ffaf
Xenoker <xenoker@users.noreply.github.com>
2023-12-09 12:23:24 -0500

    Day 9

--- 9.py
+++ 9.py
@@ -6,10 +6,9 @@ L = [list(map(int,l.split())) for l in getlines(9)]
 def part(p2=False):
     S = 0
     for l in L:
-        s,j = [l],0
+        s = [l[::[1,-1][p2]]]
         while any(s[0]): s.insert(0,[b-a for a,b in pairwise(s[0])])
-        for si in [x[[-1,0][p2]] for x in s]: j=si+([j,-j][p2])
-        S+=j
+        S += sum(x[-1] for x in s)
     return S
 
 if __name__ == '__main__':
